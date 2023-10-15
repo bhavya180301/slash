367b24bf036265b0a3f2ffa5daef83e20281a5a6
yash chandrani <ychandr@ncsu.edu>
2023-10-15 18:08:52 -0400

    Project restructuring to fix the db issue during register

--- src/modules/app.py
+++ src/modules/app.py
@@ -1,11 +1,23 @@
-from webapp import app, db
-from flask import request, render_template, redirect, url_for
+from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 from scraper import driver
-from webapp.forms import RegisterForm
-from src.modules.webapp.models import Users
+from forms import RegisterForm
+
+app = Flask(__name__, template_folder=".")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY']='504038774627ae2489c38028'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(length=50),nullable=False)
+    email = db.Column(db.String(length=100), nullable=False, unique=True)
+    password = db.Column(db.String(length=30), nullable=False)
+
+
 @app.route("/")
 def landingpage():
-    return render_template("./static/landing.html")
+    return render_template("./webapp/static/landing.html")
 
 
 @app.route("/search", methods=["POST", "GET"])
@@ -16,7 +28,7 @@ def product_search(new_product="", sort=None, currency=None, num=None):
 
     data = driver(product, currency, num, 0, False, None, True, sort)
 
-    return render_template("./static/result.html", data=data, prod=product)
+    return render_template("./webapp/static/result.html", data=data, prod=product)
 
 
 @app.route("/filter", methods=["POST", "GET"])
@@ -45,17 +57,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('landingpage'))
-    return render_template("./static/register.html", form=form)
+    return render_template("./webapp/static/register.html", form=form)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        user1 = Users(name="Yash", email="y@yahhoo.com", password="1234")
-        db.session.add(user1)
-        db.session.commit()
-
-
-        users = Users.query.all()
-        print(users)
-    app.run(debug=True)
+# app.run(debug=True)
 
--- src/modules/webapp/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__, template_folder=".")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY']='504038774627ae2489c38028'
-db = SQLAlchemy(app)
-
