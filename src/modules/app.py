d8b2a8d82bdd5ec5d1a507ca183241681b5503be
Ashaka Mehta <ashakamehta8@gmail.com>
2023-10-19 23:58:08 -0400

    update requirements.txt

--- src/modules/app.py
+++ src/modules/app.py
@@ -15,7 +15,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 from flask_login import login_user, LoginManager, UserMixin, logout_user, current_user
-from scraper import driver
 
 from sqlalchemy.exc import IntegrityError
 
